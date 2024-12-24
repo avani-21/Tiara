8375fb4043b696dfd9fe51040264abdb5bea8913
Avani-github <avanipoonthottam@gmail.com>
2024-12-24 22:46:45 +0530

    updated code

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,13 +5,17 @@ dotenv.config();
 
 const connectdb=async ()=>{
     try{
-         const connect=await mongoose.connect('mongodb://localhost:27017/Tiara')
+         const connect=await mongoose.connect("mongodb+srv://avanipoonthottam:bOdZIjh5DPnr0dF6@cluster0.850re.mongodb.net/Tiara")
 
+       
+         console.log(connect);
+         
       console.log(`MongoDb connected:${connect.connection.host}`);
 
     }catch(error){
         console.error(error);
-        process.exist(1)
+        process.exit();
+
     }
 }
 
